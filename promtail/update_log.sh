325b88c7561b4dd0b2936739f495303fc223d118
xiechengqi <xiechengqiemail@163.com>
2021-11-17 17:10:39 +0800

    add

---
+++ promtail/update_log.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#
+# 2021/11/17
+# xiechengqi
+# 更新 promtail 需要监控的 log 软链接
+#
+
+logPath='/data/log'
+mkdir -p ${logPath} || exit 1
+
+log_regex_array=('/var/log/containers/window-post-miner-32g-mainnet-*.log' '/var/log/containers/winning-post-miner-32g-mainnet-*.log' '/var/log/containers/daemon-mainnet-*.log' '/var/log/containers/seal-miner-32g-mainnet-*.log')
+
+for log_regex in ${log_regex_array[*]}
+do
+ls ${log_regex} &> /dev/null && ln -fs ${log_regex} ${logPath} || continue
+done
