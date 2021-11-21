17958a103af76010b8b734a198fcc8b27962200a
Xiechengqi <Xiechengqi01@gmail.com>
2021-11-21 19:32:05 +0800

    Update 10.9.7.173.sh

--- crontab/10.9.7.173.sh
+++ crontab/10.9.7.173.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 promtail_update_log
+
+ps axu | grep '/data/client-exporter/client-exporter.py' &> /dev/null || nohup python3 /data/client-exporter/client-exporter.py &> /dev/null &
