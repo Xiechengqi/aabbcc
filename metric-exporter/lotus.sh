f5eca40f6cff9086058ecce5004025b9a03032d4
Xiechengqi <Xiechengqi01@gmail.com>
2021-11-22 17:38:15 +0800

    Update lotus.sh

--- metric-exporter/lotus.sh
+++ metric-exporter/lotus.sh
@@ -14,13 +14,13 @@ ps axu | grep -v grep | grep fuse_dfs &> /dev/null
 lotus_storage_health=$?
 
 # 构建 prometheus textfile
-cat > ${metricPath}/.lotus << EOF
+cat > ${metricPath}/.lotus-metric << EOF
 # HELP lotus_storage_health get lotus storage health
 # TYPE lotus_storage_health gauge
 lotus_storage_health{hostname="$(hostname)"} ${lotus_storage_health}
 EOF
 
-mv ${metricPath}/.lotus ${metricPath}/lotus.prom
+mv ${metricPath}/.lotus-metric ${metricPath}/lotus.prom
 }
 
 main $@
