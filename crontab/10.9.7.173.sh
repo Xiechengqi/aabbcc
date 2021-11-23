88536e3951f900423d98bd2f63b0c484c9d6bc81
Xiechengqi <Xiechengqi01@gmail.com>
2021-11-23 11:02:53 +0800

    Update 10.9.7.173.sh

--- crontab/10.9.7.173.sh
+++ crontab/10.9.7.173.sh
@@ -2,4 +2,6 @@
 
 promtail_update_log
 
-ps axu | grep '/data/client-exporter/client-exporter.py' | grep -v grep || nohup python3 /data/client-exporter/client-exporter.py &> /dev/null &
+lotus_metric
+
+windowPost_metric
