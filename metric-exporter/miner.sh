2e011a6a4e98c4afc716c5494d6542e4f3c7fb01
xiechengqi <xiechengqiemail@163.com>
2021-12-09 11:51:38 +0800

    update

--- metric-exporter/miner.sh
+++ metric-exporter/miner.sh
@@ -25,8 +25,8 @@ lotus_move_storage_sector_id=`grep 'MoveStorage done' /var/log/containers/seal-m
 [ ".${lotus_move_storage_sector_id}" = "." ] && lotus_move_storage_sector_id="0"
 lotus_move_storage_spend_time_tmp=`grep 'MoveStorage done' /var/log/containers/seal-miner-32g-mainnet-*.log | grep -E '[0-9]+m[0-9]' | tail -1 | awk -F 'elapse ' '{print $NF}' | awk -F '.' '{print $1}'`
 [ ".${lotus_move_storage_spend_time_tmp}" = "." ] && lotus_move_storage_spend_time="0"
-lotus_move_storage_spend_time_minute=`echo ${lotus_move_storage_spend_time} | awk -F 'm' '{print $1}'`
-lotus_move_storage_spend_time_second=`echo ${lotus_move_storage_spend_time} | awk -F 'm' '{print $NF}'`
+lotus_move_storage_spend_time_minute=`echo ${lotus_move_storage_spend_time_tmp} | awk -F 'm' '{print $1}'`
+lotus_move_storage_spend_time_second=`echo ${lotus_move_storage_spend_time_tmp} | awk -F 'm' '{print $NF}'`
 lotus_move_storage_spend_time=`echo "${lotus_move_storage_spend_time_minute}*60 + ${lotus_move_storage_spend_time_second}" | bc`
 
 # 构建 prometheus textfile
@@ -37,8 +37,8 @@ miner_p1_fail_num{ip="${ip}", hostname="$(hostname)"} ${miner_p1_fail_num}
 # HELP lotus_move_storage_sector_id get lotus move storage sector id 
 # TYPE lotus_move_storage_sector_id gauge
 lotus_move_storage_sector_id{ip="${ip}", hostname="$(hostname)"} ${lotus_move_storage_sector_id}
-# HELP lotus_move_storage_time get lotus move storage spend time
-# TYPE lotus_move_storage_time gauge
+# HELP lotus_move_storage_spend_time get lotus move storage spend time
+# TYPE lotus_move_storage_spend_time gauge
 lotus_move_storage_spend_time{ip="${ip}", hostname="$(hostname)"} ${lotus_move_storage_spend_time}
 EOF
 
