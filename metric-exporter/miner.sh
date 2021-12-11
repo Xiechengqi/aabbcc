76105182c208aa24aabc00a8b7a40947088ecc84
xiechengqi <xiechengqiemail@163.com>
2021-12-11 14:03:08 +0800

    update

--- metric-exporter/miner.sh
+++ metric-exporter/miner.sh
@@ -2,7 +2,7 @@
 
 #
 # xiechengqi
-# 2021/12/09
+# 2021/12/11
 # get miner metric
 #
 
@@ -15,6 +15,9 @@ fi
 function main() {
 
 ip=`hostname -I | awk '{print $1}'`
+hostName=`hostname`
+nowHour=`date +%H`
+nowMin=`date +%M`
 
 # 收集指标
 # sealPreCommit1Failed number metric
@@ -33,13 +36,46 @@ lotus_move_storage_spend_time=`echo "${lotus_move_storage_spend_time_minute}*60 
 cat > ${metricPath}/.miner-metric << EOF
 # HELP miner_p1_fail_num get sealPreCommit1Failed number
 # TYPE miner_p1_fail_num gauge
-miner_p1_fail_num{ip="${ip}", hostname="$(hostname)"} ${miner_p1_fail_num}
+miner_p1_fail_num{ip="${ip}", hostname="${hostName}"} ${miner_p1_fail_num}
 # HELP lotus_move_storage_spend_time get lotus move storage spend time
 # TYPE lotus_move_storage_spend_time gauge
-lotus_move_storage_spend_time{ip="${ip}", hostname="$(hostname)", sector_id="${lotus_move_storage_sector_id}"} ${lotus_move_storage_spend_time}
+lotus_move_storage_spend_time{ip="${ip}", hostname="${hostName}", sector_id="${lotus_move_storage_sector_id}"} ${lotus_move_storage_spend_time}
 EOF
 
+if [ "$nowMin" = "14" ] && [ "$nowMin" = "10" ]
+then
+
+# seal miner container
+containerName=`docker ps | grep seal-miner | grep -v pause | head -1 | awk '{print $NF}'`
+
+# create prometheus textfile
+cat > ${metricPath}/.expired_sector-metric << EOF
+# HELP lotus_expired_sector get lotus expired sector
+# TYPE lotus_expired_sector gauge
+EOF
+
+# collect p1 fail sector id from log
+grep -i 'sealPreCommit1Failed' /var/log/containers/seal-miner-32g-mainnet-* | grep 'acquire' | awk -F '{' '{print $NF}' | awk -F '}' '{print $1}' | awk '{print $NF}' | sort | uniq > /tmp/$$_p1_fail_sector_id_list
+
+# check sector, if in ~/.lotusminer/unsealed or no finalize in log, skip it
+for sectorId in `cat /tmp/$$_p1_fail_sector_id_list`
+do
+! docker exec -it ${containerName} ls ~/.lotusminer/unsealed | grep "${sectorId} " &> /dev/null && docker exec -it ${containerName} lotus-miner sectors status --log ${sectorId} | grep -i 'finalize' &> /dev/null
+if [ "$?" = "0" ]
+then
+expire_date=`docker exec -it ${containerName} lotus-miner sectors status --log ${sectorId} | grep -i 'finalize' | tail -1 | awk -F '+' '{print $1}' | awk -F '.' '{print $NF}'`
+expired_timestamp=`date -d "${expire_date}" +%s`
+now_timestamp=`date +%s`
+expired_time=`echo "(${now_timestamp} - ${expired_timestamp}) / 3600" | bc`
+cat >> ${metricPath}/.expired_sector-metric << EOF
+lotus_expired_sector{ip="${ip}", hostname="${hostName}", expired_time="${expired_time}"} ${sectorId}
+EOF
+fi
+done
+
 mv ${metricPath}/.miner-metric ${metricPath}/miner-metric.prom
+fi
+
 }
 
 main $@
