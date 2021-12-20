28fcb88b9663f1cc33d2c4054f04f2a8c9ecaa0e
Xiechengqi <Xiechengqi01@gmail.com>
2021-12-20 15:46:38 +0800

    Update get_lucky_value.sh

--- scripts/get_lucky_value.sh
+++ scripts/get_lucky_value.sh
@@ -9,4 +9,4 @@
 
 minerId=$1
 [ ".${minerId}" = "." ] && echo "Usage: curl -SsL https://raw.githubusercontent.com/Xiechengqi/aabbcc/master/scripts/get_lucky_value.sh | bash -s [minerId]"
-printf %.2f $(echo "$(curl -SsL 'https://filfox.info/api/v1/address/${minerId}/mining-stats?duration=24h' | jq -r .luckyValue) * 100" | bc)
+printf %.2f $(echo "$(curl -SsL https://filfox.info/api/v1/address/${minerId}/mining-stats?duration=24h | jq -r .luckyValue) * 100" | bc)
