e5ed55998856885398769312686401ccd9b8faae
Xiechengqi <Xiechengqi01@gmail.com>
2021-12-28 19:03:50 +0800

    Update lotus.sh

--- metric-exporter/lotus.sh
+++ metric-exporter/lotus.sh
@@ -18,7 +18,7 @@ ip=$(hostname -I | awk '{print $1}')
 
 # collect metric
 ## check fuse process status, aliyun -> fuse_dfs; qiniu -> fcfs.sh
-ps axu | grep -v grep | grep fuse_dfs &> /dev/null || ps aux | grep -v grep | grep 'fcfs -e' &> /dev/null
+ps axu | grep -v grep | grep fuse_dfs &> /dev/null || ps aux | grep -v grep | grep 'fcfs' &> /dev/null
 lotus_storage_health=$?
 
 # create prometheus textfile
