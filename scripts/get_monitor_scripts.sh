0238c5417159c8ba953f18ba75b566f81c726d75
Xiechengqi <Xiechengqi01@gmail.com>
2022-02-07 15:17:15 +0800

    Create get_monitor_scripts.sh

---
+++ scripts/get_monitor_scripts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#
+# 2022/02/07
+# xiechengqi
+# install monitor scripts
+# usage: curl | bash -s [minerId]
+#
+
+minerId=$1
+
+cat > /usr/local/bin/lotus-exporter-farcaster.py << EOF
+/usr/local/bin/lotus-exporter-farcaster.py > /var/lib/prometheus/node-exporter/farcaster.prom.$$
+sed -i "s/${minerId}/$(hostname)/g" /var/lib/prometheus/node-exporter/farcaster.prom.$$
+mv /var/lib/prometheus/node-exporter/farcaster.prom.$$ /var/lib/prometheus/node-exporter/farcaster.prom
+mv /var/lib/prometheus/node-exporter/farcaster.prom /data/
+mv /data/farcaster.prom /data/metrics
+EOF
