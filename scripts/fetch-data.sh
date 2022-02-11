5d9630f46cd85bf0dc7a875479667a805256c848
Xiechengqi <Xiechengqi01@gmail.com>
2022-02-11 20:19:23 +0800

    Update fetch-data.sh

--- scripts/fetch-data.sh
+++ scripts/fetch-data.sh
@@ -1,5 +1,13 @@
-# daemon
-/usr/local/bin/lotus-exporter-farcaster.py > /var/lib/prometheus/node-exporter/farcaster.prom.$$
-sed -i "s/@MINERID/@HOSTNAME/g" /var/lib/prometheus/node-exporter/farcaster.prom.$$
+#!/usr/bin/env bash
 
-mv /var/lib/prometheus/node-exporter/farcaster.prom.$$ /var/lib/prometheus/node-exporter/farcaster.prom
+#
+# 2022/02/11
+# xiechengqi
+# fetch lotus info to node-exporter prom file
+#
+
+tmpMetircPath="/data/metric/farcaster.prom.$$"
+metricPath="/data/metric/farcaster.prom"
+
+/usr/local/bin/lotus-exporter-farcaster.py | sed -i "s/@MINERID/@HOSTNAME/g" > ${tmpMetircPath}
+mv /data/metric/farcaster.prom.$$ /data/metric/farcaster.prom
