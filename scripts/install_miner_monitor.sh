501bf349e365032c3f369755fd5611e7a24507c8
Xiechengqi <Xiechengqi01@gmail.com>
2022-02-12 17:03:59 +0800

    Update install_miner_monitor.sh

--- scripts/install_miner_monitor.sh
+++ scripts/install_miner_monitor.sh
@@ -13,18 +13,22 @@ miner_id=$1
 miner_hostname=$2
 miner_url=$3
 miner_token=$4
-daemon_url=$5
+daemon_url="$5"
 daemon_token=$6
 
 # download lotus exporter and modify api info
 curl -SsL https://raw.githubusercontent.com/Xiechengqi/aabbcc/master/scripts/lotus-exporter-farcaster.py -o /usr/local/bin/lotus-exporter-farcaster.py
 chmod +x /usr/local/bin/lotus-exporter-farcaster.py
-sed -i "s#@MINER_URL#${miner_url}#g; s/@MINER_TOKEN/${miner_token}/g; s/@DAEMON_URL#${daemon_url}#g; s/@DAEMON_TOKEN/${daemon_token}/g" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#@MINER_URL#${miner_url}#g" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#@MINER_TOKEN#${miner_token}#g" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#@DAEMON_URL#${daemon_url}#g" /usr/local/bin/lotus-exporter-farcaster.py
+sed -i "s#@DAEMON_TOKEN#${daemon_token}#g" /usr/local/bin/lotus-exporter-farcaster.py
 
 # modify metric file
 curl -SsL https://raw.githubusercontent.com/Xiechengqi/aabbcc/master/scripts/fetch-data.sh -o /usr/local/bin/fetch-data.sh
 chmod +x /usr/local/bin/fetch-data.sh
-sed -i "s#@MINERID#${miner_id}#g; s#@HOSTNAME#${miner_hostname}#g" /usr/local/bin/fetch-data.sh
+sed -i "s#@MINERID#${miner_id}#g" /usr/local/bin/fetch-data.sh
+sed -i "s#@HOSTNAME#${miner_hostname}#g" /usr/local/bin/fetch-data.sh
 
 # add crontab job
 curl -SsL https://raw.githubusercontent.com/Xiechengqi/aabbcc/master/scripts/lotus-exporter-farcaster -o /etc/cron.d/lotus-exporter-farcaster
