e0801f350542ab87dff5a4f4d3a4918b6f2283df
Xiechengqi <Xiechengqi01@gmail.com>
2022-02-13 11:16:35 +0800

    Update and rename tmp.sh to t.sh

--- scripts/t.sh
+++ scripts/t.sh
@@ -1,3 +1,6 @@
+num=`hostname -I | awk '{print $1}' | awk -F '.' '{print $NF}'`
+minerId='miner'$(echo "${num} + 100" | bc)
+
 hostnamectl set-hostname ${minerId} && hostnamectl --pretty && hostnamectl --static && hostnamectl --transient
 yes | kubeadm reset
 kubeadm join 10.19.5.20:6443 --token d6dbql.16w5gros799914to --discovery-token-ca-cert-hash sha256:7391f5afc8d209d685f1142cc43fab93934834ddd71ac37cff12f85097ccc061
