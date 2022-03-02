a3907d72b8538b4c56e7e0f0d613d79148534f6b
Xiechengqi <Xiechengqi01@gmail.com>
2022-03-02 13:02:17 +0800

    Update and rename init-10.sh to init-11.sh

--- scripts/init-10.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/mem_reserved =.*/mem_reserved = \"0\"/g' lotus-exporter-farcaster.py
---
+++ scripts/init-11.sh
@@ -0,0 +1,1 @@
+sed -i 's/mem_reserved =.*/mem_reserved = \"0\"/g' /usr/local/bin/lotus-exporter-farcaster.py
