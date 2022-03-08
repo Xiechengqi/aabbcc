0fd93083c4c946126e5ae8eac1b412257fe861fd
Xiechengqi <Xiechengqi01@gmail.com>
2022-03-08 12:25:19 +0800

    Update and rename init-00.sh to init_0.sh

--- scripts/init-00.sh
+++
@@ -1,1 +0,0 @@
-echo "| $(hostname -I | awk '{print $1}') | $(hostname) | $(cat /proc/cpuinfo | grep processor | wc -l) | / | $(free -h | grep -E '^Mem:' | awk '{print $2}' |"
---
+++ scripts/init_0.sh
@@ -0,0 +1,1 @@
+echo "| $(hostname -I | awk '{print $1}') | $(hostname) | $(cat /proc/cpuinfo | grep processor | wc -l) | / | $(free -h | grep -E '^Mem:' | awk '{print $2}') |"
