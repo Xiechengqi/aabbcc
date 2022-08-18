a34490c9b8d268c3212fad7377338f10d0297f2c
Xiechengqi <Xiechengqi01@gmail.com>
2022-08-18 16:02:23 +0800

    Update height2date.sh

--- scripts/height2date.sh
+++ scripts/height2date.sh
@@ -8,4 +8,4 @@
 
 block_height=$1
 block_height_timestamp=$(echo "${block_height} * 30 + 1598313600" | bc)
-echo -e "Block height: ${block_height}\nDate: $(date "+%Y-%m-%d %H:%M:%S" -d @${block_height_timestamp})"
+echo -e "Block height: ${block_height}\nDate: $(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S" -d @${block_height_timestamp})"
